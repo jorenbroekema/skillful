507a9034ff98a19de525406b4b016c0fa37d934c
jorenbroekema <joren.broekema@gmail.com>
2019-04-07 18:42:22 +0200

    add difficulty and dates to workshop component

--- resources/views/workshops/components/workshop.blade.php
+++ resources/views/workshops/components/workshop.blade.php
@@ -3,7 +3,7 @@
     <a style="line-height: 36px;" href="/workshops/{{ $id }}">{{ $title }}</a>
     @if($canEdit == "true")
       <button
-        class="float-right btn btn-primary"
+        class="float-right btn btn-secondary"
         data-toggle="modal"
         data-target="#workshopEditModal-{{ $id }}"
       >Edit</button>
@@ -11,23 +11,61 @@
   </div>
   <div class="card-body">
     <div>{{ $description }}</div>
-    @if (Auth::check())
-      @if ($isParticipating == 'false')
-        <form method="POST" action="/participants/{{ Auth::id() }}">
-          @method('PATCH')
-          @csrf
-          <input type="hidden" name="workshop" value="{{ $id }}">
-          <button type="button" onclick="this.form.submit()" class="btn btn-primary mt-3">Participate</button>
-        </form>
-      @else
-        <form method="POST" action="/participants/{{ Auth::id() }}">
-          @method('DELETE')
-          @csrf
-          <input type="hidden" name="workshop" value="{{ $id }}">
-          <button type="button" onclick="this.form.submit()" class="btn btn-danger mt-3">Unlist</button>
-        </form>
-      @endif
-    @endif
+    <div class="mt-2">
+      By: <a href="#">Joren Broekema</a>
+    </div>
+  </div>
+  <div class="card-body">
+    <div class="row justify-content-between">
+      <div class="col-md-auto">
+        <div class="row text-center">
+          <div class="col date date--start">
+            <div class="date__heading text-uppercase h6">Start date</div>
+            <div class="date__card shadow p-4 bg-white rounded">
+              <div class="h4 bold"><strong>08</strong></div>
+              <div>Jun 18</div>
+              <div class="mt-2"><em>13:00</em></div>
+            </div>
+          </div>
+          <div class="col date date--end">
+            <div class="date__heading text-uppercase h6">End date</div>
+            <div class="date__card shadow p-4 bg-white rounded">
+              <div class=" h4 bold"><strong>08</strong></div>
+              <div>Jun 18</div>
+              <div class="mt-2"><em>16:00</em></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-auto align-self-end">
+        <div class="d-flex flex-column align-items-end mt-3">
+          <div class="d-flex align-items-end">
+            <div class="bg-warning ml-1" style="width: 10px; height: 10px; border-radius: 5px;"></div>
+            <div class="bg-warning ml-1" style="width: 10px; height: 20px; border-radius: 5px;"></div>
+            <div class="bg-secondary ml-1" style="width: 10px; height: 30px; border-radius: 5px;"></div>
+          </div>
+          @if (Auth::check())
+            <div class="participate-button mt-4">
+              @if ($isParticipating == 'false')
+                <form method="POST" action="/participants/{{ Auth::id() }}">
+                  @method('PATCH')
+                  @csrf
+                  <input type="hidden" name="workshop" value="{{ $id }}">
+                  <button type="button" onclick="this.form.submit()" class="btn btn-primary">Participate</button>
+                </form>
+              @else
+                <form method="POST" action="/participants/{{ Auth::id() }}">
+                  @method('DELETE')
+                  @csrf
+                  <input type="hidden" name="workshop" value="{{ $id }}">
+                  <button type="button" onclick="this.form.submit()" class="btn btn-danger">Unlist</button>
+                </form>
+              @endif
+            </div>
+          @endif
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
